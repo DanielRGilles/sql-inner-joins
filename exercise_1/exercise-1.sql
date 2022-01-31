63ee6fa809161de57aaf117692c7f1913207e3be
DanielRGilles <danryangilles@gmail.com>
2022-01-31 12:43:22 -0800

    first 2 complete

--- exercise_1/exercise-1.sql
+++ exercise_1/exercise-1.sql
@@ -1,2 +1,7 @@
 -- find all address_id of addresses in the city of Dundee
 -- use INNER JOIN FROM address on to city
+SELECT address_id 
+FROM address
+INNER JOIN city
+ON city.city_id = address.city_id 
+WHERE city IN ('Dundee')
--- exercise_2/exercise-2.sql
+++ exercise_2/exercise-2.sql
@@ -1,2 +1,7 @@
 -- find all rental dates for customers with the first_name Patricia
 -- use INNER JOIN FROM customer onto rental
+SELECT *
+FROM rental
+INNER JOIN customer
+ON customer.customer_id = rental.customer_id 
+WHERE first_name IN ('Patricia')
